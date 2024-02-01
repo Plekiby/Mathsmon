573687558ff30f5d4fb9358981d8610066e42bf4
Art-Fey <94886321+Art-Fey@users.noreply.github.com>
2024-02-01 21:12:26 +0100

    Tentative Portail

--- Assets/Scipts/PlayerMouvement.cs
+++ Assets/Scipts/PlayerMouvement.cs
@@ -62,4 +62,5 @@ public class PlayerController : MonoBehaviour
         }
         return true;
     }
+
 }
---
+++ Assets/Scipts/SceneManagement/Portal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Player entered the portal");
+        if (collision.tag == "Player") {
+            print("Switching scene to" + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
