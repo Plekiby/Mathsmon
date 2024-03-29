6ddf13bdce017280fa34c0545bf15716fbaca89f
Art-Fey <94886321+Art-Fey@users.noreply.github.com>
2024-03-29 10:16:04 +0100

    Ajout des fonctions mathématiques

--- Assets/Scipts/Maths/Addition.cs
+++ Assets/Scipts/Maths/Addition.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class Addition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int number1;
+    private int number2;
+    private int numberAnswer;
+
+
+
+    private int getnumber1()
     {
-        
+        return this.number1;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void setnumber1(int number1) { 
+        this.number1 = number1;
     }
+
+
+
+
+    private int getnumber2()
+    {
+        return this.number2;
+    }
+    private void setnumber2(int number2) { 
+        this.number2 = number2;
+    }
+
+
+
+    private int getAnswer()
+    {
+        return this.numberAnswer;
+    }
+    
+    private void setAnswer(int number1)
+    {
+        this.numberAnswer = number1;
+    }
+
+
+
+
+    private void CalculateAnswer()
+    {
+        numberAnswer = number2 * number1;  
+    }
+
+   
+    public void SimpleQuestion()
+    {
+        if (int.TryParse(_inputField.text, out int userAnswer) && userAnswer == numberAnswer)
+        {
+            _textFields[2].text = "Correct!";
+        }
+        else
+        {
+            _textFields[2].text = "Wrong, the correct answer is " + numberAnswer;
+        }
+    }
+
+
+
+
 }
