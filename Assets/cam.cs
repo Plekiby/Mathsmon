aae43b2a0ce3b41f4a53fd1954c340fc1660a379
Plekiby <plekibyelila@gmail.com>
2024-03-29 10:16:34 +0100

    cam route et pharma

---
+++ Assets/cam.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class cam : MonoBehaviour
+{
+    public GameObject player;
+    public float timeOffset;
+    public Vector3 posOffset;
+
+    private Vector3 velocity;
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
+    }
+}
+
