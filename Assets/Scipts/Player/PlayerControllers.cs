46752b6f47f71fb6ffda8584f2726532463598ee
Art-Fey <94886321+Art-Fey@users.noreply.github.com>
2024-04-26 01:13:55 +0200

    Fixage certains bugs

--- Assets/Scipts/Player/PlayerControllers.cs
+++ Assets/Scipts/Player/PlayerControllers.cs
@@ -280,4 +280,9 @@ public class PlayerControllers : MonoBehaviour
             return pokemon;
         }
 
+    public void retourLobby()
+    {
+        SavePlayer();
+        SceneManager.LoadScene("Menu");
+    }
 }
